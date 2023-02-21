efff07acb6e94a77d70e0d6513ca18cfd42c2652
mknyambura <kariuki.mercie@gmail.com>
2023-02-21 12:57:51 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var button = document.querySelector('#button')
+var businessNumber = document.querySelector('.businessNumber')
+var tillNumber = document.querySelector('.tillNumber')
+var accountNumber = document.querySelector('.accountNumber')
+var popUp = document.querySelector('#popUp')
+
 function leftClick(){
     button.style.left = '0'
+    businessNumber.style.display = 'block'
+    tillNumber.style.display = 'none'
+    accountNumber.style.display = 'block'
 }
 function rightClick(){
     button.style.left = '135px'
+    businessNumber.style.display = 'none'
+    tillNumber.style.display = 'block'
+    accountNumber.style.display = 'none'
+}
+function openPopup(){
+    popUp.classList.add('active');
+}
+function closePopup(){
+    popUp.classList.remove('active')
 }
